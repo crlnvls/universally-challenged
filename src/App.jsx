a2fe97f35d3942d1eced6f42a48ee277d7f685f7
“crlnvls” <cveloso@hotmail.co.uk>
2022-09-05 22:55:22 +0100

    Import all pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,20 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import {
-  Home,
-  NotFound,
-  ScoreSingle,
-  CreateRoom,
-  ScoreMulti,
-  Subjects,
-} from "./pages";
+
+import * as Pages from "./pages";
 import { DataProvider } from "./context/dataContext";
 
 function App() {
   return (
     <DataProvider>
       <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="create-room" element={<CreateRoom />}></Route>
-        <Route path="/category" element={<Subjects />}></Route>
-        <Route path="/score-single" element={<ScoreSingle />}></Route>
-        <Route path="score-multi" element={<ScoreMulti />}></Route>
-        <Route path="*" element={<NotFound />}></Route>
+        <Route path="/" element={<Pages.Home />}></Route>
+        <Route path="create-room" element={<Pages.CreateRoom />}></Route>
+        <Route path="/category" element={<Pages.Subjects />}></Route>
+        <Route path="/waiting" element={<Pages.Waiting />}></Route>
+        <Route path="/score-single" element={<Pages.ScoreSingle />}></Route>
+        <Route path="score-multi" element={<Pages.ScoreMulti />}></Route>
+        <Route path="*" element={<Pages.NotFound />}></Route>
       </Routes>
     </DataProvider>
   );
