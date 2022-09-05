6e30159c30a3ed879f505330077afe010e9205ba
vivian <vivian.v18723@gmail.com>
2022-09-05 22:47:17 +0100

    refactor passing test

--- src/components/MultiPlayerScores/MultiPlayerScores.test.jsx
+++ src/components/MultiPlayerScores/MultiPlayerScores.test.jsx
@@ -9,9 +9,11 @@ import "@testing-library/jest-dom";
 import MultiPlayerScores from ".";
 
 describe("MultiPlayerScores component", () => {
-  it("Maps through all 'h1' & checks role is 'heading'", () => {
+  beforeEach(() => {
     render(<MultiPlayerScores />);
+  });
 
+  it("Maps through all 'h1' & checks role is 'heading'", () => {
     const heading = screen.queryAllByRole("heading");
 
     heading.map((singleHeadingInArr) => {
