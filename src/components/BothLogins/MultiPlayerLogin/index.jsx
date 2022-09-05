3f0d1aaaa4362df3e02efcc90474ed3dd055d9e8
vivian <vivian.v18723@gmail.com>
2022-09-05 13:52:11 +0100

    create state variables for inputs create functions to stop page refreshing create functions to handle inputs on change add values dynamically to inputs using onChange

--- src/components/BothLogins/MultiPlayerLogin/index.jsx
+++ src/components/BothLogins/MultiPlayerLogin/index.jsx
@@ -1,19 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 
 const MultiPlayerLogin = () => {
+  // state variable for 'create room' inputs
+  const [inputValues, setInputValues] = useState({
+    username: "",
+    room: "",
+    playerNum: 0
+  });
+  console.log("multiPlayer: ", inputValues);
+
+  // username entered inside its input
+  const handleUsernameInput = (e) => {
+    setInputValues({ ...inputValues, username: e.target.value });
+  };
+
+  // room entered inside its input
+  const handleRoomInput = (e) => {
+    setInputValues({ ...inputValues, room: e.target.value });
+  };
+
+  // num of players entered inside its input
+  const handlePlayerNumInput = (e) => {
+    setInputValues({ ...inputValues, playerNum: e.target.value });
+  };
+
+  // stops page refreshing
+  const handleSubmitForm = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <>
       <h1>Create your room</h1>
 
-      <form>
+      <form onSubmit={handleSubmitForm}>
         <label htmlFor="username">Choose your Username</label>
-        <input type="text" placeholder="Enter username" id="username" />
+
+        <input
+          type="text"
+          placeholder="Enter username"
+          id="username"
+          value={inputValues.username}
+          onChange={handleUsernameInput}
+        />
 
         <label htmlFor="room">Choose your room</label>
-        <input type="text" placeholder="Enter room" id="room" />
+        <input
+          type="text"
+          placeholder="Enter room"
+          id="room"
+          value={inputValues.room}
+          onChange={handleRoomInput}
+        />
 
         <label htmlFor="numOfPlayers">How many players?</label>
-        <select name="numOfPlayers" id="numOfPlayers">
+        <select
+          name="numOfPlayers"
+          id="numOfPlayers"
+          value={inputValues.playerNum}
+          onChange={handlePlayerNumInput}
+        >
           <option value="1">1</option>
           <option value="2">2</option>
           <option value="3">3</option>
--- src/components/BothLogins/SinglePlayerLogin/index.jsx
+++ src/components/BothLogins/SinglePlayerLogin/index.jsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SinglePlayerLogin = () => {
+  // state variable for single username input
+  const [inputValue, setInputValue] = useState({ username: "" });
+  console.log("singlePlayer: ", inputValue);
+
+  // stops page refreshing
+  const handleSubmitForm = (e) => {
+    e.preventDefault();
+  };
+
+  // username entered inside input
+  const handleUsernameInput = (e) => {
+    setInputValue({ username: e.target.value });
+  };
+
   return (
     <>
       <h1>Choose your username</h1>
-      <form>
-        <input type="text" placeholder="Enter username" />
+      <form onSubmit={handleSubmitForm}>
+        <input
+          type="text"
+          placeholder="Enter username"
+          value={inputValue.username}
+          onChange={handleUsernameInput}
+        />
         <button>Done</button>
       </form>
     </>
