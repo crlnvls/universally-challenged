fdfee568de7bb8b488e625352ecc2f910038e079
vivian <vivian.v18723@gmail.com>
2022-09-05 11:20:00 +0100

    export MultiPlayerScores component

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -2,3 +2,4 @@ export { default as Questions } from "./Questions";
 export { default as Form } from "./Form";
 export { default as SinglePlayerLogin } from "./BothLogins/SinglePlayerLogin";
 export { default as MultiPlayerLogin } from "./BothLogins/MultiPlayerLogin";
+export { default as MultiPlayerScores } from "./BothScores/MultiPlayerScores";
