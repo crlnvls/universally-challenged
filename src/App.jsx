fdc0f05007291fa7cbf16ada654060b614075916
vivian <vivian.v18723@gmail.com>
2022-09-05 10:39:09 +0100

    import & use CreateRoom component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
+import { CreateRoom } from "./pages";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div className="App"></div>;
+  return (<div className="App">
+    <CreateRoom/>
+  </div>);
 }
 
 export default App;
