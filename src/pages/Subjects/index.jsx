c5109f2527386dcdcddeeaf7dfa8fff0f6f03db1
“crlnvls” <cveloso@hotmail.co.uk>
2022-09-05 14:30:51 +0100

    Add the form in the subject page

--- src/pages/Subjects/index.jsx
+++ src/pages/Subjects/index.jsx
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 
 import Form from "../../components/Form";
 
 const Subjects = () => {
   return (
     <>
-      <h1>Choose your subject</h1>
+      <h1>Choose your category</h1>
+      <Form />
     </>
   );
 };
