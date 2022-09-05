d03ca464881826ad69bd690ac74515cdccf8ebe5
vivian <vivian.v18723@gmail.com>
2022-09-05 11:02:33 +0100

    import & use MultiPlayerLogin component

--- src/pages/CreateRoom/index.jsx
+++ src/pages/CreateRoom/index.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { SinglePlayerLogin } from "../../components";
+import { MultiPlayerLogin, SinglePlayerLogin } from "../../components";
 
 const CreateRoom = () => {
   return (
     <>
       <SinglePlayerLogin />
+      <MultiPlayerLogin />
     </>
   );
 };
