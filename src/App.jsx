55c1cceac0bbaa89f22e096390d5dcd4cb89c832
“crlnvls” <cveloso@hotmail.co.uk>
2022-09-05 17:01:52 +0100

    Fixed the paths names

--- src/App.jsx
+++ src/App.jsx
@@ -15,13 +15,11 @@ function App() {
     <DataProvider>
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        <Route path="createRoom" element={<CreateRoom />}></Route>
-
-        <Route path="/category" element={<Subjects />}></Route>   
-
-        <Route path="/ScoreSingle" element={<ScoreSingle />}></Route>      
-        <Route path="scoreMulti" element={<ScoreMulti />}></Route>
-        <Route path="*" element={<NotFound />}></Route>      
+        <Route path="create-room" element={<CreateRoom />}></Route>
+        <Route path="/category" element={<Subjects />}></Route>
+        <Route path="/score-single" element={<ScoreSingle />}></Route>
+        <Route path="score-multi" element={<ScoreMulti />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
       </Routes>
     </DataProvider>
   );
