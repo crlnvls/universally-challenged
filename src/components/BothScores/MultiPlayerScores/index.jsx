f17d8556c967d9ae3f5be85623f89d8ab08a3e17
vivian <vivian.v18723@gmail.com>
2022-09-05 11:17:00 +0100

    create & export MultiPlayerScores component

---
+++ src/components/BothScores/MultiPlayerScores/index.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const MultiPlayerScores = () => {
+  return (
+    <>
+      <h1>Winner</h1>
+
+      <h3>shaquille.oatmeal.</h3>
+      <i class="fa-solid fa-trophy"></i>
+
+      <ul>
+        <li>
+          shaquille.oatmeal. <span>4/5</span>
+        </li>
+        <li>
+          google_was_my_idea. <span>3/5</span>
+        </li>
+        <li>
+          averagestudent <span>1/5</span>
+        </li>
+      </ul>
+
+      <button>Play again</button>
+      <button>Return Home</button>
+    </>
+  );
+};
+
+export default MultiPlayerScores;
