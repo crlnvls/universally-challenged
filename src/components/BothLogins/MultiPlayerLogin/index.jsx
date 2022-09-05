4768b33fc07af6cb7d5081d7bfb5b2ba6337a148
vivian <vivian.v18723@gmail.com>
2022-09-05 19:53:20 +0100

    use react links to link to waiting page add conditional checks to ensure... ...number is between 2 & 5 inclusively, form is hidden... ...&  next btn is shown on completion

--- src/components/BothLogins/MultiPlayerLogin/index.jsx
+++ src/components/BothLogins/MultiPlayerLogin/index.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const MultiPlayerLogin = () => {
   /* state variables for: 'create room' inputs, form submitting, form validation */
@@ -9,7 +10,7 @@ const MultiPlayerLogin = () => {
   });
   const [submitForm, setSubmitForm] = useState(false);
   const [isValid, setIsValid] = useState(false);
-  // console.log("multiPlayer: ", inputValue);
+  // console.log("multiplayer: ", inputValues);
 
   const handleUsernameInput = (e) => {
     // updates username input
@@ -35,7 +36,14 @@ const MultiPlayerLogin = () => {
       return prev;
     });
 
-    if (inputValues.username && inputValues.room && inputValues.playerNum) {
+    // ensures input values meet correct conditions for submission
+    if (
+      inputValues.username &&
+      inputValues.room &&
+      inputValues.playerNum &&
+      parseInt(inputValues.playerNum) >= 2 &&
+      parseInt(inputValues.playerNum) <= 5
+    ) {
       setIsValid((prev) => {
         prev = true;
         return prev;
@@ -51,49 +59,63 @@ const MultiPlayerLogin = () => {
         <p style={{ color: "green" }}>Thank you for creating your room</p>
       ) : null}
 
-      <form onSubmit={handleSubmitForm}>
-        <label htmlFor="username">Choose your Username</label>
+      {submitForm && isValid ? null : (
+        <form onSubmit={handleSubmitForm}>
+          <label htmlFor="username">Choose your Username</label>
 
-        <input
-          type="text"
-          placeholder="Enter username"
-          id="username"
-          value={inputValues.username}
-          onChange={handleUsernameInput}
-        />
-        {submitForm && !inputValues.username ? (
-          <p style={{ color: "red" }}>Please enter a username</p>
-        ) : null}
-        <br />
+          <input
+            type="text"
+            placeholder="Enter username"
+            id="username"
+            value={inputValues.username}
+            onChange={handleUsernameInput}
+          />
+          {submitForm && !inputValues.username ? (
+            <p style={{ color: "red" }}>Please enter a username</p>
+          ) : null}
+          <br />
 
-        <label htmlFor="room">Choose your room</label>
-        <input
-          type="text"
-          placeholder="Enter room"
-          id="room"
-          value={inputValues.room}
-          onChange={handleRoomInput}
-        />
-        {submitForm && !inputValues.room ? (
-          <p style={{ color: "red" }}>Please enter a room</p>
-        ) : null}
-        <br />
+          <label htmlFor="room">Choose your room</label>
+          <input
+            type="text"
+            placeholder="Enter room"
+            id="room"
+            value={inputValues.room}
+            onChange={handleRoomInput}
+          />
+          {submitForm && !inputValues.room ? (
+            <p style={{ color: "red" }}>Please enter a room</p>
+          ) : null}
+          <br />
 
-        <label htmlFor="numOfPlayers">How many players?</label>
-        <input
-          type="number"
-          id="numOfPlayers"
-          value={inputValues.playerNum}
-          onChange={handlePlayerNumInput}
-        />
+          <label htmlFor="numOfPlayers">How many players (2-5)?</label>
+          <input
+            type="number"
+            placeholder="Enter number of players"
+            id="numOfPlayers"
+            value={inputValues.playerNum}
+            onChange={handlePlayerNumInput}
+          />
 
-        {submitForm && !inputValues.playerNum ? (
-          <p style={{ color: "red" }}>Please enter the number of players</p>
-        ) : null}
-        <br />
+          {(submitForm && !inputValues.playerNum) ||
+          (submitForm && inputValues.playerNum.includes(".")) ||
+          (submitForm && parseInt(inputValues.playerNum) < 2) ||
+          (submitForm && parseInt(inputValues.playerNum) > 5) ? (
+            <p style={{ color: "red" }}>
+              Please select a whole number between 2 and 5
+            </p>
+          ) : null}
+          <br />
 
-        <button>Done</button>
-      </form>
+          {submitForm && isValid ? null : <button>Submit</button>}
+        </form>
+      )}
+
+      {submitForm && isValid ? (
+        <Link to={"/waiting"}>
+          <button>Next</button>
+        </Link>
+      ) : null}
     </>
   );
 };
