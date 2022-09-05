bab0444882abdd20ba165db7a2128a554d2b74b4
vivian <vivian.v18723@gmail.com>
2022-09-05 12:17:32 +0100

    import Routes & Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
+
 import { CreateRoom, ScoreMulti } from "./pages";
+import "./App.css";
 
 function App() {
   return (
