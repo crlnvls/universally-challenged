1acca8141b6e2dd4b83246143c693a8a7ca8cabb
vivian <vivian.v18723@gmail.com>
2022-09-05 11:19:29 +0100

    import & use ScoreMulti component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
-import { CreateRoom } from "./pages";
+import { CreateRoom, ScoreMulti } from "./pages";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return (<div className="App">
-    <CreateRoom/>
-  </div>);
+  return (
+    <div className="App">
+      <CreateRoom />
+      <ScoreMulti />
+    </div>
+  );
 }
 
 export default App;
