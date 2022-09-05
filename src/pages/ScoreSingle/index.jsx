14c8f9bcaa7ad997440f33c50eb26c44f4dd2588
“crlnvls” <cveloso@hotmail.co.uk>
2022-09-05 16:14:10 +0100

    Fixed issues from merging

--- src/pages/ScoreSingle/index.jsx
+++ src/pages/ScoreSingle/index.jsx
@@ -1,20 +1,18 @@
 import React from "react";
 
-
-  return <>
-    <h1>Your Score:</h1>
-    // Pseudo-code to diplay results from quiz round
-
-    <h2>Previous Scores:</h2>
-    // Pseudo-code to diplay results from previous quiz rounds
-
-    //onClick navigate back to quiz start
-    <button>Play Again</button>
-    //onClick navigate to home page
-    <button>Return Home</button>
-
-    </>;
-
+const ScoreSingle = () => {
+  return (
+    <>
+      <h1>Your Score:</h1>
+      // Pseudo-code to diplay results from quiz round
+      <h2>Previous Scores:</h2>
+      // Pseudo-code to diplay results from previous quiz rounds //onClick
+      navigate back to quiz start
+      <button>Play Again</button>
+      //onClick navigate to home page
+      <button>Return Home</button>
+    </>
+  );
 };
 
 export default ScoreSingle;
--- src/pages/Waiting/index.jsx
+++ src/pages/Waiting/index.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 
 const Waiting = () => {
-
   return <div>Score</div>;
-
-
+};
 export default Waiting;
