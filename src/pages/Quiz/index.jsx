640d5addf91b113a9e0e1ecbf5bdad0f8dd13093
vivian <vivian.v18723@gmail.com>
2022-09-07 00:16:41 +0100

    import question component for navigation purposes... ...from quiz page

--- src/pages/Quiz/index.jsx
+++ src/pages/Quiz/index.jsx
@@ -1,10 +1,19 @@
 import React, { useContext } from "react";
+
+import { Questions } from "../../components";
 import DataContext from "../../context/dataContext";
 
 const Quiz = () => {
   const { questionData, subject } = useContext(DataContext);
 
-  return <div>Quiz</div>;
+  return (
+    <>
+      <div>Quiz</div>
+
+      {/* vivian added this so this page links to the category page 🙂 */}
+      <Questions />
+    </>
+  );
 };
 
 export default Quiz;
