5d9ecddd64833d2c5864f96d5c08f86aa7f21272
vivian <vivian.v18723@gmail.com>
2022-09-06 20:12:28 +0100

    create separate component for multi player logins

---
+++ src/pages/CreateMultiRoom/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { MultiPlayerLogin } from "../../components";
+
+const CreateMultiRoom = () => {
+  return (
+    <>
+      <MultiPlayerLogin />
+    </>
+  );
+};
+
+export default CreateMultiRoom;
+
+// for both multiplayer and single: render components based on previous choice
