3777f8780253e37c20b956e47ceac43e081e6140
vivian <vivian.v18723@gmail.com>
2022-09-07 00:15:55 +0100

    add navigation to both single & multi score page

--- src/components/Questions/index.jsx
+++ src/components/Questions/index.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { useNavigate } from "react-router-dom"; /* vivian added this so this page links to the category page 🙂 */
 
 const Questions = () => {
-  return <div>Questions</div>;
+  /* vivian added this so this page links to the category page 🙂 */
+  const navigate = useNavigate();
+  function moveToNextPageSingle() {
+    navigate("/score-single");
+  }
+  function moveToNextPageMulti() {
+    navigate("/scoreMulti");
+  }
+
+  return (
+    <>
+      <div>Questions</div>
+
+
+
+      {/* vivian added this so this page links to the category page 🙂 */}
+      <button onClick={moveToNextPageSingle}>For single</button>
+      <button onClick={moveToNextPageMulti}>For Multi</button>
+    </>
+  );
 };
 
 export default Questions;
