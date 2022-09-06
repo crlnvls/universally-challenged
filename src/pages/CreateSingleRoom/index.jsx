8d0aed72e5252564d29c158dfabeed0fa6ee226c
vivian <vivian.v18723@gmail.com>
2022-09-06 20:12:49 +0100

    create separate component for single player logins

---
+++ src/pages/CreateSingleRoom/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { SinglePlayerLogin } from "../../components";
+
+const CreateSingleRoom = () => {
+  return (
+    <>
+      <SinglePlayerLogin />
+    </>
+  );
+};
+
+export default CreateSingleRoom;
+
+// for both multiplayer and single: render components based on previous choice
