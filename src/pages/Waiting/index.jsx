24aaa4380e5620e2fcc76bc16ddfef45ff7b7001
“crlnvls” <cveloso@hotmail.co.uk>
2022-09-06 09:38:41 +0100

    Experimenting with css

--- src/pages/Waiting/index.jsx
+++ src/pages/Waiting/index.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import "./Waiting.css";
 
 const Waiting = () => {
-  return <div>Score</div>;
+  return (
+    <>
+      <div className="waiting-body">
+        <h1 className="header-waiting">Waiting Room </h1>
+        <div className="container-pacman">
+          <div className="path"></div>
+          <div className="path"></div>
+          <div className="path"></div>
+          <div className="path"></div>
+          <div className="path"></div>
+          <div className="path"></div>
+          <div className="path"></div>
+          <div className="path"></div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Waiting;
