a95aa2dd421a24d2a8efcb0d3e780186b0833bab
vivian <vivian.v18723@gmail.com>
2022-09-07 17:11:43 +0100

    add placeholder question options

--- src/components/Questions/Sports/index.jsx
+++ src/components/Questions/Sports/index.jsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
+
+// import DataContext from "../../../context/dataContext";
 
 const Sports = () => {
+  // const { handleSubmit, getQuiz } = useContext(DataContext);
+
   return (
     <>
-      <h1>Sports</h1>
+      <section>
+        <h1>Sports</h1>
+        <h3>Question here</h3>
+        <aside>
+          <button>Option 1</button>
+          <button>Option 2</button>
+          <button>Option 3</button>
+          <button>Option 4</button>
+        </aside>
+        <p>Q.1</p>
+      </section>
     </>
   );
 };
