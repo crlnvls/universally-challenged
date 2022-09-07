142818ebb95ef06aa64474630b7a0bab348eb1b3
“crlnvls” <cveloso@hotmail.co.uk>
2022-09-07 19:53:15 +0100

    Change create-room route

--- src/pages/ScoreSingle/index.jsx
+++ src/pages/ScoreSingle/index.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from "react-router-dom";
 import DataContext from "../../context/dataContext";
 
 const ScoreSingle = () => {
@@ -8,23 +8,27 @@ const ScoreSingle = () => {
 
   const navigate = useNavigate();
   const navigateToSinglePlayer = () => {
-    navigate('/createRoom')
-  }
+    navigate("/create-room");
+  };
   const navigateHome = () => {
-    navigate('/')
-  }
+    navigate("/");
+  };
 
   return (
     <>
       <h1>{inputValue["username"]} Score:</h1>
 
       <h2>Previous Scores:</h2>
-  
-    {/* onClick navigate back to quiz start */}
-    <button className="btn" onClick={navigateToSinglePlayer}>Play Again</button>
 
-    {/* onClick navigate to home page */}
-    <button className="btn" onClick={navigateHome}>Return Home</button>
+      {/* onClick navigate back to quiz start */}
+      <button className="btn" onClick={navigateToSinglePlayer}>
+        Play Again
+      </button>
+
+      {/* onClick navigate to home page */}
+      <button className="btn" onClick={navigateHome}>
+        Return Home
+      </button>
     </>
   );
 };
