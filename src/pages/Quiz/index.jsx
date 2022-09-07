861a1ecd9b44915fddb70ee6a33567808fddfe27
“crlnvls” <cveloso@hotmail.co.uk>
2022-09-07 17:31:59 +0100

    Add navigate single-score

--- src/pages/Quiz/index.jsx
+++ src/pages/Quiz/index.jsx
@@ -1,4 +1,5 @@
 import React, { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import DataContext from "../../context/dataContext";
 import { Subject, Question, AnswerForm, QNumber } from "../../components";
 
@@ -7,6 +8,7 @@ const Quiz = () => {
   const [answers, setAnswers] = useState([]);
   const [score, setScore] = useState(0);
   const [currentQuestion, setCurrentQuestion] = useState(0);
+  const navigate = useNavigate();
 
   const calcScore = () => {
     if (answers == questionData[currentQuestion]["correct_answer"]) {
@@ -19,17 +21,20 @@ const Quiz = () => {
     setAnswers(e.target.value);
     setCurrentQuestion(currentQuestion + 1);
     calcScore();
+    if (currentQuestion + 1 == questionData.length) {
+      navigate("/score-single");
+    }
   };
   return (
     <>
       <Subject questionData={questionData} />
-      <Question />
+      <Question currentQuestion={currentQuestion} questionData={questionData} />
       <AnswerForm
         handleAnswer={handleAnswer}
         currentQuestion={currentQuestion}
         questionData={questionData}
       />
-      <QNumber />
+      <QNumber currentQuestion={currentQuestion} questionData={questionData} />
     </>
   );
 };
