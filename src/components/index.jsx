225aa16b680153bfd8f67b5ac2b0b95e34bcc92f
“crlnvls” <cveloso@hotmail.co.uk>
2022-09-08 18:31:17 +0100

    Add all scores to components

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -6,3 +6,4 @@ export { default as Form } from "./Form";
 export { default as SinglePlayerLogin } from "./BothLogins/SinglePlayerLogin";
 export { default as MultiPlayerLogin } from "./BothLogins/MultiPlayerLogin";
 export { default as MultiPlayerScores } from "./MultiPlayerScores";
+export { default as AllScores } from "./AllScores";
