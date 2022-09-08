e7d8f25b380fd2e2d2588a8218772e2ee1f95973
vivian <vivian.v18723@gmail.com>
2022-09-08 12:00:22 +0100

    add more tests

--- src/components/BothLogins/SinglePlayerLogin/SinglePlayerLogin.test.js
+++ src/components/BothLogins/SinglePlayerLogin/SinglePlayerLogin.test.js
@@ -2,92 +2,202 @@
  * @jest-environment jsdom
  */
 
-import React from "react";
-import * as router from "react-router";
 import { screen, render } from "@testing-library/react";
 import "@testing-library/jest-dom";
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import * as router from "react-router";
 
 import SinglePlayerLogin from ".";
 
-const navigate = jest.fn();
-
-beforeEach(() => {
-  jest.spyOn(router, "useNavigate").mockImplementation(() => navigate);
-});
-
-describe("SinglePlayerLogin component element roles", () => {
-  beforeEach(() => {
-    render(<SinglePlayerLogin inputValue={{ username: "Test" }} />);
-  });
-
-  describe("To check roles of elements", () => {
-    it("Maps through all 'h1' & checks role is 'heading'", () => {
-      const heading = screen.queryAllByRole("heading");
-
-      heading.map((singleHeadingInArr) => {
-        expect(singleHeadingInArr).toBeInTheDocument();
-      });
-    });
-
-    it("Checks form element role is 'form", () => {
-      const form = screen.queryByRole("form");
-      expect(form).toBeInTheDocument();
-    });
-
-    it("Checks input element role is 'input", () => {
-      const input = screen.queryByRole("input");
-      expect(input).toBeInTheDocument();
-    });
-
-    it("Checks p element role is 'warningPara", () => {
-      const paraElem = screen.queryByRole("warningPara");
-      expect(paraElem).not.toBeInTheDocument();
-    });
-
-    it("Checks button element role is 'submit", () => {
-      const button = screen.queryByRole("submit");
-      expect(button).toBeInTheDocument();
-    });
-  });
-});
-
-describe("Running SinglePlayerLogin component", () => {
-  let component;
-
-  beforeEach(() => {
-    component = render(<SinglePlayerLogin />);
-  });
-
+describe("Running component", () => {
   it("Renders without crashing", () => {
-    component;
+    render(
+      <BrowserRouter>
+        <SinglePlayerLogin />
+      </BrowserRouter>
+    );
   });
 });
 
-describe("Attribute testing", () => {
+describe("Component element roles", () => {
+  beforeEach(() => {
+    render(
+      <BrowserRouter>
+        <SinglePlayerLogin />
+      </BrowserRouter>
+    );
+  });
+
+  it("Maps through all 'h1' to check roles = 'heading'", () => {
+    const heading = screen.queryAllByRole("heading");
+
+    heading.map((singleHeadingInArr) => {
+      expect(singleHeadingInArr).toBeInTheDocument();
+      expect(singleHeadingInArr).toBeTruthy();
+    });
+  });
+
+  it("Check one 'h1' role = 'heading", () => {
+    const heading = screen.queryByRole("heading");
+    expect(heading).toBeInTheDocument();
+    expect(heading).toBeTruthy();
+  });
+
+  it("Checks 'form' element role = 'form", () => {
+    const form = screen.queryByRole("form");
+    expect(form).toBeInTheDocument();
+    expect(form).toBeTruthy();
+  });
+
+  it("Checks 'input' element role = 'input", () => {
+    const input = screen.queryByRole("input");
+    expect(input).toBeInTheDocument();
+    expect(input).toBeTruthy();
+  });
+
+  it("Checks 'p' element role = 'warningPara", () => {
+    const paraElem = screen.queryByRole("warningPara");
+    expect(paraElem).not.toBeInTheDocument();
+    expect(paraElem).toBeFalsy();
+  });
+
+  it("Checks 'button' element role = 'submit", () => {
+    const button = screen.queryByRole("submit");
+    expect(button).toBeInTheDocument();
+    expect(button).toBeTruthy();
+  });
+});
+
+describe("Id & Class attribute testing", () => {
+  const navigate = jest.fn();
+
+  beforeEach(() => {
+    jest.spyOn(router, "useNavigate").mockImplementation(() => navigate);
+  });
+
   test("should find 'h1' id attribute", () => {
     const { container } = render(<SinglePlayerLogin />);
-    console.log("LOOK==> ", container.outerHTML);
     const mockComponent = container.querySelector("h1#singlePlayerTitle");
     expect(mockComponent).toBeInTheDocument();
+    expect(mockComponent).toBeTruthy();
   });
 
-  test("should find form id attribute", () => {
+  test("should find 'form' id attribute", () => {
     const { container } = render(<SinglePlayerLogin />);
     const mockComponent = container.querySelector(
       "form#singlePlayerFormContainer"
     );
     expect(mockComponent).toBeInTheDocument();
+    expect(mockComponent).toBeTruthy();
   });
 
-  test("should find input id attribute", () => {
+  test("should find 'input' id attribute", () => {
     const { container } = render(<SinglePlayerLogin />);
     const mockComponent = container.querySelector("input#username");
     expect(mockComponent).toBeInTheDocument();
+    expect(mockComponent).toBeTruthy();
   });
 
-  test("should find button class attribute", () => {
+  test("should find 'button' class attribute", () => {
     const { container } = render(<SinglePlayerLogin />);
     const mockComponent = container.querySelector("button.btn");
     expect(mockComponent).toBeInTheDocument();
+    expect(mockComponent).toBeTruthy();
+  });
+});
+
+describe("Other selector testing", () => {
+  const navigate = jest.fn();
+
+  beforeEach(() => {
+    jest.spyOn(router, "useNavigate").mockImplementation(() => navigate);
+  });
+
+  test("should find 'h1' selector", () => {
+    const { container } = render(<SinglePlayerLogin />);
+    const mockComponent = container.querySelector("h1");
+    expect(mockComponent).toBeInTheDocument();
+    expect(mockComponent).toBeTruthy();
+  });
+
+  test("should find 'input' selector", () => {
+    const { container } = render(<SinglePlayerLogin />);
+    const mockComponent = container.querySelector("input");
+    expect(mockComponent).toBeInTheDocument();
+    expect(mockComponent).toBeTruthy();
+  });
+
+  test("should find 'input[name='username']' selector", () => {
+    const { container } = render(<SinglePlayerLogin />);
+    const mockComponent = container.querySelector("input[name='username']");
+    expect(mockComponent).toBeInTheDocument();
+    expect(mockComponent).toBeTruthy();
+  });
+
+  test("should find 'input[autoComplete='off']' selector", () => {
+    const { container } = render(<SinglePlayerLogin />);
+    const mockComponent = container.querySelector("input[autoComplete='off']");
+    expect(mockComponent).toBeInTheDocument();
+    expect(mockComponent).toBeTruthy();
+  });
+
+  test("should find 'input[type='text']' selector", () => {
+    const { container } = render(<SinglePlayerLogin />);
+    const mockComponent = container.querySelector("input[type='text']");
+    expect(mockComponent).toBeInTheDocument();
+    expect(mockComponent).toBeTruthy();
+  });
+
+  test("should find 'input[placeholder='Enter username']' selector", () => {
+    const { container } = render(<SinglePlayerLogin />);
+    const mockComponent = container.querySelector(
+      "input[placeholder='Enter username']"
+    );
+    expect(mockComponent).toBeInTheDocument();
+    expect(mockComponent).toBeTruthy();
+  });
+
+  test("should find 'button' selector", () => {
+    const { container } = render(<SinglePlayerLogin />);
+    const mockComponent = container.querySelector("button");
+    expect(mockComponent).toBeInTheDocument();
+    expect(mockComponent).toBeTruthy();
+  });
+});
+
+describe("Component text content", () => {
+  beforeEach(() => {
+    render(
+      <BrowserRouter>
+        <SinglePlayerLogin />
+      </BrowserRouter>
+    );
+  });
+
+  it("Shows correct text in heading", () => {
+    const text = screen.queryByRole("heading");
+    expect(text.textContent).toBe("Choose your username");
+    expect(text.textContent).toBeTruthy();
+  });
+
+  it("Shows correct text in input placeholder", () => {
+    const placeholderText = screen
+      .getByPlaceholderText("Enter username")
+      .getAttribute("placeholder");
+    expect(placeholderText).toBe("Enter username");
+    expect(placeholderText).toBeTruthy();
+  });
+
+  it("Shows correct text as input value", () => {
+    const inputText = screen.queryByRole("input");
+    expect(inputText.textContent).toBeFalsy();
+  });
+
+  it("Shows correct text on button", () => {
+    const btnText = screen.queryByRole("submit");
+    console.log("LOOK==> ", btnText);
+    expect(btnText.textContent).toBeTruthy();
+    expect(btnText.textContent).toBe("Submit");
   });
 });
