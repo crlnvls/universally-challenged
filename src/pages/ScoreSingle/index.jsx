edea5b5e5ed37f77508060b5f6110f5f102f87ad
“crlnvls” <cveloso@hotmail.co.uk>
2022-09-08 18:29:14 +0100

    Add styles to single score page

--- src/pages/ScoreSingle/index.jsx
+++ src/pages/ScoreSingle/index.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import DataContext from "../../context/dataContext";
 import "./ScoreSingle.css";
+import { NavLink } from "react-router-dom";
 
 const ScoreSingle = () => {
   const { inputValue, score } = useContext(DataContext);
@@ -18,7 +19,28 @@ const ScoreSingle = () => {
     <>
       <h1 className="header-name">{inputValue["username"]}</h1>
       <h2 className="scores"> Score: {score}</h2>
+      <div className="container-pacman">
+        <span class="loader"></span>
+      </div>
       <div className="container-score">
+        <div className="container-nav">
+          <NavLink to="/leaderboard" className="link-leader">
+            <img
+              src="/alien.png"
+              alt="start button"
+              className="img-nav"
+              width={30}
+            />{" "}
+            Leaderboard
+            <img
+              src="/alien.png"
+              alt="start button"
+              width={30}
+              className="img-nav"
+            />
+          </NavLink>
+        </div>
+
         {/* onClick navigate back to quiz start */}
         <button className="btn-score" onClick={navigateToSinglePlayer}>
           Play Again
